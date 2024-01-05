44db025d370b7935cb1a4ab52b259fc8593e465f
Rakholiya Raj Kishorbhai <rakholiyaraj468@gmail.com>
2024-01-05 09:44:23 +0530

    commit

--- event_management/admin.py
+++ event_management/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from.models import CustomUser,UserGroup,Event,EventInvitation
+from.models import CustomUser,UserGroup,Event,EventInvitation,Product,Cart,CartItem
 
 # Register your models here.
 admin.site.register(CustomUser)
 admin.site.register(UserGroup)
 admin.site.register(Event)
 admin.site.register(EventInvitation)
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(CartItem)
 
